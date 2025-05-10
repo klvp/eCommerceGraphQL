fbc5cb166b37dbbda5807a7ab743f2df92500800
Kuna Lakshmi Vara Prasad <kunalakshmi.prasad@nw18.com>
2025-05-10 20:26:57 +0530

    add support to insert dump in mongoDB

--- src/db/setupCollection.js
+++ src/db/setupCollection.js
@@ -2,57 +2,97 @@ import { db } from "./index.js";
 import fs from 'fs';
 import csv from 'csv-parser';
 import { UUID } from "mongodb";
+import path from 'path';
 
-async function run() {
-    const ordersCollection = db.collection('orders');
+function processFile(filePath, collectionName){
+    return new Promise((resolve,reject) => {
+        const results = [];
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (row) => {
+                try {
+                    let order = {}
+                    if (collectionName == "orders") {
+                        const productsStr = row.products.replace(/'/g, '"');
+                        const products = JSON.parse(productsStr);
 
-    const results = [];
+                        order = {
+                            _id: new UUID(row._id),
+                            customerId: new UUID(row.customerId),
+                            products: products.map(p => ({
+                                productId: new UUID(p.productId),
+                                quantity: Number(p.quantity),
+                                priceAtPurchase: parseFloat(p.priceAtPurchase)
+                            })),
+                            totalAmount: parseFloat(row.totalAmount),
+                            orderDate: new Date(row.orderDate),
+                            status: row.status
+                        };
 
-    fs.createReadStream(process.cwd() + "/dump/orders.csv")
-        .pipe(csv())
-        .on('data', (row) => {
-            try {
-                // Convert stringified 'products' to real array
-                const productsStr = row.products.replace(/'/g, '"');
-                const products = JSON.parse(productsStr);
+                        if (!order.customerId || !Array.isArray(order.products)) {
+                            throw new Error('Invalid data');
+                        }
+                    } else if(collectionName == "products") {
+                        order = {
+                            _id: new UUID(row._id),
+                            name: row.name,
+                            category: row.category,
+                            price: parseFloat(row.price),
+                            stock: Number(row.stock),
+                        }
+                    } else {
+                        order = {
+                            _id: new UUID(row._id),
+                            name: row.name,
+                            email: row.email,
+                            age: Number(row.age),
+                            location: row.location,
+                            gender: row.gender,
+                        }
+                    }
 
-                const order = {
-                    _id: new UUID(row._id),
-                    customerId: new UUID(row.customerId),
-                    products: products.map(p => ({
-                        productId: new UUID(p.productId),
-                        quantity: Number(p.quantity),
-                        priceAtPurchase: parseFloat(p.priceAtPurchase)
-                    })),
-                    totalAmount: parseFloat(row.totalAmount),
-                    orderDate: new Date(row.orderDate),
-                    status: row.status
-                };
-
-                // Simple validation
-                if (!order.customerId || !Array.isArray(order.products)) {
-                    throw new Error('Invalid data');
+                    results.push(order);
+                } catch (err) {
+                    console.error(`Skipping row due to error: ${err.message}`);
                 }
-
-                results.push(order);
-            } catch (err) {
-                console.error(`Skipping row due to error: ${err.message}`);
-            }
-        })
-        .on('end', async () => {
-            try {
-                if (results.length) {
-                    const insertResult = await ordersCollection.insertMany(results);
-                    console.log(`Inserted ${insertResult.insertedCount} orders`);
-                } else {
-                    console.log('No valid orders to insert');
+            })
+            .on('end', async () => {
+                try {
+                    if (results.length) {
+                        const collection = db.collection(collectionName);
+                        const insertResult = await collection.insertMany(results);
+                        console.log(`Inserted ${insertResult.insertedCount}`);
+                    } else {
+                        console.log('No valid orders to insert');
+                    }
+                    resolve();
+                } catch (err) {
+                    console.error('Error inserting into MongoDB:', err);
+                    reject();
                 }
-            } catch (err) {
-                console.error('Error inserting into MongoDB:', err);
-            } finally {
-                process.exit(0)
-            };
-        })
+            })
+            .on('error', reject);
+    })
 }
 
-run().catch(console.dir);
+async function run() {
+
+    const folderPath = path.join(process.cwd(), "dump")
+
+    const files = fs.readdirSync(path.join(process.cwd(), "dump")).filter(file => {
+        return fs.statSync(path.join(folderPath, file)).isFile();
+    });
+
+    for (const file of files) {
+        await processFile(path.join(folderPath, file), file.replace(".csv", ""))
+    }
+
+}
+
+run().then(() => {
+    console.log("All files processed.");
+    process.exit(0);
+}).catch(err => {
+    console.error("Error in run:", err);
+    process.exit(1);
+});
